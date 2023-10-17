de16482315a32b01f44396e83cb2b7c0efe273f5
kh <127534515+kh5402@users.noreply.github.com>
2023-10-18 01:40:10 +0900

    Update makeRSS_NogizakaBlog.py

--- makeRSS_NogizakaBlog.py
+++ makeRSS_NogizakaBlog.py
@@ -87,6 +87,7 @@ for url_and_xml in url_and_xmls:
         # 次のページへのリンクがあるかチェック
         next_page_pattern = re.compile(r'<a href="(/s/n46/diary/MEMBER/list\?ima=\d+&amp;page=\d+&amp;ct=\d+&amp;cd=MEMBER)">')
         next_page_match = next_page_pattern.search(html_content)
+        print(next_page_match)
         if next_page_match:
             url = 'https://www.nogizaka46.com' + next_page_match.group(1)
         else:
