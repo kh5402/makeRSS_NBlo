8772525677c609dd352b74543d946eb746334713
kh <127534515+kh5402@users.noreply.github.com>
2023-10-18 00:50:55 +0900

    Update makeRSS_NogizakaBlog.py

--- makeRSS_NogizakaBlog.py
+++ makeRSS_NogizakaBlog.py
@@ -42,50 +42,50 @@ url_and_xmls = [
 for url_and_xml in url_and_xmls:
     url = url_and_xml['url']
     xml_file_name = url_and_xml['xml']
-    include_phrase = url_and_xml['include_phrase']
+    include_phrase = url_and_xml.get('include_phrase', [])
 
-    existing_links = set()
-    
-    if os.path.exists(xml_file_name):
-        tree = ET.parse(xml_file_name)
-        root = tree.getroot()
-    else:
-        root = ET.Element("rss", version="2.0")
-        channel = ET.SubElement(root, "channel")
-        ET.SubElement(channel, "title").text = "Latest Blogs"
-        ET.SubElement(channel, "description").text = "Nogizaka46 Latest Blog Posts"
+    # XMLの初期化
+    root = Element("rss", version="2.0")
+    channel = SubElement(root, "channel")
+    SubElement(channel, "title").text = "Latest Blogs"
+    SubElement(channel, "description").text = "Nogizaka46 Latest Blog Posts"
 
-    for item in root.findall(".//item/link"):
-        existing_links.add(item.text)
+    while url:  # URLがNoneになるまで続ける
+        print(f"Fetching URL: {url}")  # 確認用
 
-    response = requests.get(url)
-    html_content = unescape(response.text)
-    article_pattern = re.compile(r'<a class="bl--card js-pos a--op hv--thumb" href="([^"]+)">')
-    title_pattern = re.compile(r'<p class="bl--card__ttl">([^<]+)<\/p>')
-    date_pattern = re.compile(r'<p class="bl--card__date">([^<]+)<\/p>')
+        response = requests.get(url)
+        html_content = response.text
 
-    for link_match, title_match, date_match in zip(article_pattern.findall(html_content), title_pattern.findall(html_content), date_pattern.findall(html_content)):
-        link = "https://www.nogizaka46.com" + link_match
-        title = title_match
-        date = date_match
+        # 記事を見つける
+        link_pattern = re.compile(r'<a class="bl--card js-pos a--op hv--thumb" href="([^"]+)">')
+        title_pattern = re.compile(r'<p class="bl--card__ttl">([^<]+)</p>')
+        date_pattern = re.compile(r'<p class="bl--card__date">([^<]+)</p>')
 
-        if any(phrase in title for phrase in include_phrase):
-            if link in existing_links:
-                continue
+        links = link_pattern.findall(html_content)
+        titles = title_pattern.findall(html_content)
+        dates = date_pattern.findall(html_content)
 
-            channel = root.find("channel")
-            new_item = ET.SubElement(channel, "item")
-            ET.SubElement(new_item, "title").text = title
-            ET.SubElement(new_item, "link").text = link
-            ET.SubElement(new_item, "pubDate").text = date
+        print(f"Found links: {links}")  # 確認用
+        print(f"Found titles: {titles}")  # 確認用
+        print(f"Found dates: {dates}")  # 確認用
 
-        # 次のページへのリンクがあるかチェック
-        next_page_match = re.search(r'<a [^>]*href="([^"]*)"[^>]*>.*?>\s*<\/a>', html_content)
-        if next_page_match:
-            url = 'https://www.nogizaka46.com' + next_page_match.group(1)
-        else:
-            url = None
-                
-    tree = ET.ElementTree(root)
+        for link, title, date in zip(links, titles, dates):
+            if any(phrase in title for phrase in include_phrase):
+                item = SubElement(channel, "item")
+                SubElement(item, "title").text = title
+                SubElement(item, "link").text = f"https://www.nogizaka46.com{link}"
+                SubElement(item, "pubDate").text = date
+
+        # 次のページへ
+        next_page_pattern = re.compile(r'<a href="(/s/n46/diary/MEMBER/list\?ima=\d+&amp;page=\d+&amp;ct=\d+&amp;cd=MEMBER)">')
+        next_page_match = next_page_pattern.search(html_content)
+        url = f"https://www.nogizaka46.com{next_page_match.group(1)}" if next_page_match else None
+
+        print(f"Next URL: {url}")  # 確認用
+
+    # XMLを保存
+    tree = ElementTree(root)
     with open(xml_file_name, 'wb') as f:
-        f.write(prettify(root).encode('utf-8'))
+        tree.write(f, encoding='utf-8', xml_declaration=True)
+
+print("Done!")  # 確認用
